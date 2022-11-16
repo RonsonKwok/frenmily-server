89ae34332980f61ad33b0a3ed78fe1a5c9e38cc3
Ronson Kwok <rk6866@hotmail.com>
2022-11-16 17:07:49 +0800

    debug knex migrate tables

--- migrations/20221115143337_create-transcations.ts
+++ migrations/20221115143337_create-transcations.ts
@@ -5,7 +5,7 @@ export async function up(knex: Knex): Promise<void> {
     await knex.schema.createTable("transcations", (table) => {
         table.increments();
         table.integer("debitor_id").notNullable();
-        table.foreign("debitor_id").references("user.id");
+        table.foreign("debitor_id").references("users.id");
         table.integer("creditor_id").notNullable();
         table.foreign("creditor_id").references("users.id");
         table.integer("transcations_amount").notNullable();
