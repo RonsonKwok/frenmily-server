3ddd2a2468a870f1a814ee31181f1871de8458b2
Louie Chan <lokyin0831@gmail.com>
2022-11-20 01:28:38 +0800

    add API for dividing Money

--- controllers/friends-controller.ts
+++ controllers/friends-controller.ts
@@ -23,6 +23,7 @@ export class FriendsController {
                 }
                 return false;
             });
+            console.log("unique :", unique);
 
             res.json(unique);
             return;
@@ -114,62 +115,63 @@ export class FriendsController {
         }
     };
 
-    //////////////////////////////////////// from BAD project
+    calculateMoney = async (req: Request, res: Response) => {
+        try {
+            console.log("calculateMoney API");
+            let user_id = req.body.user_id;
+            let user_friend_id = req.body.user_friend_id;
+            console.log("user_id :", user_id);
+            console.log("user_friend_id :", user_friend_id);
 
-    // me = async (req: Request, res: Response) => {
-    //     res.json({
-    //         message: 'Success retrieve user',
-    //         data: {
-    //             user: req.session['user'] ? req.session['user'] : null
-    //         }
-    //     })
-    // }
+            // check有冇人差呢個user錢
+            let results1 = await this.friendsService.calculateMoney1(
+                user_id,
+                user_friend_id
+            );
+            let userCanGet = 0;
+            for (let result1 of results1) {
+                userCanGet += result1.transcations_amount;
+            }
+            console.log(`user can get $${userCanGet} from ${user_friend_id}`);
 
-    // //uploadToAlbum + pass photo to model
-    // uploadToAlbum = async (req: Request, res: Response) => {
-    //     try {
-    //         let currentUser = req.session['user']
-    //         let { files } = await formParse(req)
-    //         for (let fieldName in files) {
-    //             await this.albumService.uploadToAlbum((files[fieldName] as any).newFilename, currentUser.id)
-    //         }
-    //         const albumResult = await this.albumService.getAlbum(currentUser.id);
+            // check user有冇差人錢
+            let results2 = await this.friendsService.calculateMoney2(
+                user_id,
+                user_friend_id
+            );
+            let userShouldPay = 0;
+            for (let result2 of results2) {
+                userCanGet += result2.transcations_amount;
+            }
+            console.log(
+                `user should pay $${userShouldPay} to ${user_friend_id}`
+            );
 
-    //         res.json(albumResult)
+            if (userCanGet - userShouldPay == 0) {
+                // case 1: 冇拖冇欠
+                res.json({
+                    case: 1,
+                    amount: 0,
+                });
+            } else if (userCanGet - userShouldPay > 0) {
+                // case 2: user can get
+                res.json({
+                    case: 2,
+                    amount: userCanGet - userShouldPay,
+                });
+            } else if (userShouldPay - userCanGet > 0) {
+                // case 3: user should pay
+                res.json({
+                    case: 3,
+                    amount: userShouldPay - userCanGet,
+                });
+            }
+            return;
+        } catch (e) {
+            console.log(e);
 
-    //         return
-    //     } catch (e) {
-    //         console.log(e);
-
-    //         res.status(400).send("Upload Fail")
-    //         return
-    //     }
-    // }
-
-    // getAlbum = async (req: Request, res: Response) => {
-    //     let currentUser = req.session['user']
-    //     const albumResult = await this.albumService.getAlbum(currentUser.id);
-
-    //     res.json([albumResult, currentUser])
-
-    //     return
-    // }
-
-    // deletePhotoFromAlbum = async (req: Request, res: Response) => {
-    //     try {
-    //         const photoName = req.body.index
-    //         console.log(photoName)
-
-    //         await this.albumService.deletePhoto(photoName)
-
-    //         res.json({
-    //             message: 'del success'
-    //         })
-    //     } catch (e) {
-    //         console.log('error : ' + e)
-    //         res.status(500).json({
-    //             message: 'del fail'
-    //         })
-    //     }
-    // }
+            res.status(400).send("add friend Fail");
+            return;
+        }
+    };
 }
