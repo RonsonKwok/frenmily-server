04c2b7fc2b35a1cf982e39c094edca84bbe8b295
Ronson Kwok <rk6866@hotmail.com>
2022-11-23 18:04:34 +0800

    update cart table list - quantity and is assigned

--- migrations/20221115095044_create-carts.ts
+++ migrations/20221115095044_create-carts.ts
@@ -8,6 +8,8 @@ export async function up(knex: Knex): Promise<void> {
         table.foreign("users_id").references("users.id");
         table.integer("goods_id").notNullable();
         table.foreign("goods_id").references("goods.id");
+        table.integer("quantity").notNullable();
+        table.boolean("is_assigned").notNullable();
       });
 }
 
