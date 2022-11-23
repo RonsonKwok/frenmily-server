44e627affe98b59974a04534f45c80c88af5f59a
Ronson Kwok <rk6866@hotmail.com>
2022-11-23 12:09:56 +0800

    amended seeds and goods table to match BigQuery

--- migrations/20221115033054_create-goods.ts
+++ migrations/20221115033054_create-goods.ts
@@ -9,13 +9,20 @@ export async function up(knex: Knex): Promise<void> {
         table.integer("category_id").notNullable();
         table.foreign("category_id").references("goods_categories.id");
         table.text("goods_picture");
+        table.string("aeon_price");
+        table.string("dch_price");
+        table.string("jasons_price");
+        table.string("parknshop_price");       
+        table.string("wellcome_price");
+        table.string("mannings_price");
+        table.string("watsons_price");
+        table.string("ztore_price");
+        table.string("report_lower_price").unsigned();
+        table.string("evidence_image");
+        table.timestamps(false, true);
     });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
     await knex.schema.dropTableIfExists("goods");
 }
-
-
-
