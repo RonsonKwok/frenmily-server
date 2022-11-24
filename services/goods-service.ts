3a94cee6bbb7400a5e2be8914ff2d6e930ccb203
Ronson Kwok <rk6866@hotmail.com>
2022-11-24 23:31:06 +0800

    amended searchkeyword query to select *

--- services/goods-service.ts
+++ services/goods-service.ts
@@ -184,7 +184,7 @@ async searchKeyword(name: string): Promise<any> {
     try {
         console.log("DATABASE: Searching Keywords");
         const results = await this.knex.raw(
-            `select name from goods
+            `select * from goods
             where name ILIKE '%${name}%';
             
         `
