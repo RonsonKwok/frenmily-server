e4124b00e43a55b029fab7764125f9550fd7ce19
Louie Chan <lokyin0831@gmail.com>
2022-12-02 17:45:45 +0800

    change image size

--- utils/upload.ts
+++ utils/upload.ts
@@ -10,7 +10,7 @@ const initFormidable = (): IncomingForm => {
     let param: Partial<Options> = {
         keepExtensions: true,
         maxFiles: 1,
-        maxFileSize: 1024 * 1024 * 100,
+        maxFileSize: 1024 * 1024 * 100000,
         filter: (part) => {
             console.log(part);
             return part.mimetype?.startsWith("image/") || false;
