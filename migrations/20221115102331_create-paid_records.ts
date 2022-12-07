caebd03d7c285330d2ad104872a17051e55ce50b
Mike Chan <kiwai0929@gmail.com>
2022-12-07 14:20:50 +0800

    fixed migration

--- migrations/20221115102331_create-paid_records.ts
+++ migrations/20221115102331_create-paid_records.ts
@@ -2,17 +2,21 @@ import { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
-    // await knex.schema.createTable("paid_records", (table) => {
-    //     table.increments();
-    //     table.integer("user_id").notNullable();
-    //     table.foreign("user_id").references("users.id");
-    //     table.integer("receipt_amount").notNullable();
-    //     table.string("receipt_image").notNullable();
-    //   });
+    await knex.schema.createTable("paid_records", (table) => {
+        table.increments();
+        table.integer("user_id").notNullable();
+        table.foreign("user_id").references("users.id");
+        table.integer("group_id").notNullable();
+        table.foreign("group_id").references("groups.id");
+        table.string("receipt_image").notNullable();
+        table.float("amount").notNullable();
+        table.string("remarks");
+        table.timestamps(false, true);
+    });
 }
 
 
 export async function down(knex: Knex): Promise<void> {
-    // await knex.schema.dropTableIfExists("paid_records");
+    await knex.schema.dropTableIfExists("paid_records");
 }
 
